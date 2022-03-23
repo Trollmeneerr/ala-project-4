8ab7d061880b3efe528f93cd72b9753e68b748a1
AlexHouwaard <94444669+AlexHouwaard@users.noreply.github.com>
2022-03-23 15:24:08 +0100

    calculations

--- scripts/jshome.js
+++ scripts/jshome.js
@@ -37,3 +37,45 @@ function plus(){
     document.getElementById("/uitkomst").innerHTML = uitkomst;
     }
     document.getElementById("deelBtn").addEventListener("click", deel);
+
+    function dakje(){ 
+        let num1 = document.getElementById("^inputOne").value;
+        let num2 = document.getElementById("^inputTwo").value;   
+        let inputOne = parseFloat(num1);
+        let inputTwo = parseFloat(num2);
+        let uitkomst = inputOne ** inputTwo;
+        document.getElementById("^uitkomst").innerHTML = uitkomst;
+        }
+        document.getElementById("^Btn").addEventListener("click", dakje);
+        
+        function procent(){ 
+        let num1 = document.getElementById("%inputOne").value;
+        let num2 = document.getElementById("%inputTwo").value;   
+        let inputOne = parseFloat(num1);
+        let inputTwo = parseFloat(num2);
+        let uitkomst = inputOne / 100 * inputTwo;  
+        document.getElementById("procentUitkomst").innerHTML = uitkomst;
+        }
+        document.getElementById("%Btn").addEventListener("click", procent);
+        
+        function keer(){ 
+        let num1 = document.getElementById("*inputOne").value;
+        let num2 = document.getElementById("*inputTwo").value;   
+        let inputOne = parseFloat(num1);
+        let inputTwo = parseFloat(num2);
+        let even = Math.round((num1 / num2), 0);
+        console.log(even);
+        document.getElementById("*uitkomst").innerHTML = uitkomst;
+        }
+        document.getElementById("keerBtn").addEventListener("click", keer);
+        
+        function deel(){ 
+        let num1 = document.getElementById("/inputOne").value;
+        let num2 = document.getElementById("/inputTwo").value;   
+        let inputOne = parseFloat(num1);
+        let inputTwo = parseFloat(num2);
+        let uitkomst = inputOne / inputTwo;
+        document.getElementById("/uitkomst").innerHTML = uitkomst;
+        }
+        document.getElementById("deelBtn").addEventListener("click", deel);
+console.log()
